f8a72cc6c7d06a662b88ef34cf6695166a417e46
German Sansores <germansansores83@gmail.com>
2024-10-02 11:55:08 -0600

    Pilas.py

--- Pilas.py
+++ Pilas.py
@@ -1,18 +1,26 @@
 class Pila:
-     def __init__(self):
-         self.items = []
+    def __init__(self):
+        self.pila = []
 
-     def estaVacia(self):
-         return self.items == []
+    def apilar(self, elemento):
+        self.pila.append(elemento)  # Agrega un elemento a la pila
 
-     def incluir(self, item):
-         self.items.append(item)
+    def desapilar(self):
+        if not self.esta_vacia():
+            return self.pila.pop()  # Elimina y devuelve el último elemento agregado
+        else:
+            return "La pila está vacía"
 
-     def extraer(self):
-         return self.items.pop()
+    def esta_vacia(self):
+        return len(self.pila) == 0  # Verifica si la pila está vacía
 
-     def inspeccionar(self):
-         return self.items[len(self.items)-1]
+    def ver_pila(self):
+        return self.pila  # Muestra el contenido de la pila
 
-     def tamano(self):
-         return len(self.items)
+# Ejemplo de uso
+p = Pila()
+p.apilar('hola')
+p.apilar(21)
+print(p.ver_pila())  # Resultado: ['hola', 21]
+p.desapilar()  # Elimina el 21
+print(p.ver_pila())  # Resultado: ['hola']
